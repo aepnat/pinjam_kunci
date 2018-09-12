001e0c8f3f567603abc4c9417ae94ff994e6cb6b
aepnat <aep.nat@gmail.com>
2018-09-12 21:31:45 +0700

    tampilan data penggunaan material

--- admin/lihat/data_penggunaan_material.php
+++ admin/lihat/data_penggunaan_material.php
@@ -1,1 +1,54 @@
-data pengunaan material
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Data</h3>
+
+        <div class="box-tools">
+          <div class="input-group input-group-sm" style="width: 150px;">
+            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+            <div class="input-group-btn">
+              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>Material</th>
+            <th>Kode</th>
+            <th>Pengguna</th>
+            <th>Perusahaan</th>
+            <th>Waktu Ambil</th>
+            <th>Aksi</th>
+          </tr>
+          <tr>
+            <td>Obeng</td>
+            <td>001</td>
+            <td>Poldi</td>
+            <td>PT. Huawei Indonesia</td>
+            <td>02 April 2018 16:00:00</td>
+            <td>
+                <button class="btn btn-xs btn-primary">Detail</button>
+                <button class="btn btn-xs btn-success">Edit</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer clearfix">
+        <ul class="pagination pagination-sm no-margin pull-right">
+          <li><a href="#">&laquo;</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">&raquo;</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.box -->
+  </div>
+</div>
