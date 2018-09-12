84f1b336854cbbbd15e54af8f990c48a96472b3d
aepnat <aep.nat@gmail.com>
2018-09-12 21:48:42 +0700

    tampilan registrasi perusahaan

--- admin/lihat/registrasi_perusahaan.php
+++ admin/lihat/registrasi_perusahaan.php
@@ -1,1 +1,34 @@
-registrasi perusahaan
+<div class="row">
+    <div class="col-md-12">
+        <!-- general form elements -->
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Formulir Registrasi Perusahaan</h3>
+          </div>
+          <!-- /.box-header -->
+          <!-- form start -->
+          <form role="form">
+            <div class="box-body">
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" class="form-control" id="nama" placeholder="Masukan Nama Perusahaan">
+              </div>
+              <div class="form-group">
+                <label for="no_telp">No. Telepon</label>
+                <input type="text" name="no_telp" class="form-control" id="no_telp" placeholder="Masukan Nomor Telepon Perusahaan">
+              </div>
+              <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <textarea name="alamat" class="form-control" id="alamat" placeholder="Masukan Alamat"></textarea>
+              </div>
+            </div>
+            <!-- /.box-body -->
+
+            <div class="box-footer">
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+          </form>
+        </div>
+        <!-- /.box -->
+    </div>
+</div>
