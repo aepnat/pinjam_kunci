46541b0b6263bd6fc5a1abce8a5f177396babd1a
aepnat <aep.nat@gmail.com>
2018-09-12 21:50:03 +0700

    Fix field name di registrasi penggunaan material

--- admin/lihat/registrasi_penggunaan_material.php
+++ admin/lihat/registrasi_penggunaan_material.php
@@ -10,20 +10,20 @@
           <form role="form">
             <div class="box-body">
               <div class="form-group">
-                <label for="id_kunci">Nama Material</label>
-                <input type="text" name="id_kunci" class="form-control" id="id_kunci" placeholder="Masukan Nama Material">
+                <label for="nm_material">Nama Material</label>
+                <input type="text" name="nm_material" class="form-control" id="nm_material" placeholder="Masukan Nama Material">
               </div>
               <div class="form-group">
-                <label for="tujuan">Kode Material</label>
-                <input type="text" name="tujuan" class="form-control" id="tujuan" placeholder="Masukan Kode Material">
+                <label for="kode_material">Kode Material</label>
+                <input type="text" name="kode_material" class="form-control" id="kode_material" placeholder="Masukan Kode Material">
               </div>
               <div class="form-group">
-                <label for="nm_peminjam">Nama Pengguna</label>
-                <input type="text" name="nm_peminjam" class="form-control" id="nm_peminjam" placeholder="Masukan Nama Pengguna">
+                <label for="nm_pengguna">Nama Pengguna</label>
+                <input type="text" name="nm_pengguna" class="form-control" id="nm_pengguna" placeholder="Masukan Nama Pengguna">
               </div>
               <div class="form-group">
-                <label for="no_telp_peminjam">No. Telp Pengguna</label>
-                <input type="text" name="no_telp_peminjam" class="form-control" id="no_telp_peminjam" placeholder="Masukan No. Telp Pengguna">
+                <label for="no_telp">No. Telp Pengguna</label>
+                <input type="text" name="no_telp" class="form-control" id="no_telp" placeholder="Masukan No. Telp Pengguna">
               </div>
               <div class="form-group">
                 <label>Jenis ID</label>
@@ -34,12 +34,12 @@
                 </select>
               </div>
               <div class="form-group">
-                <label for="email_peminjam">Nomor ID Pengguna</label>
-                <input type="text" name="email_peminjam" class="form-control" id="email_peminjam" placeholder="Masukan Nomor ID Pengguna">
+                <label for="no_id">Nomor ID Pengguna</label>
+                <input type="text" name="no_id" class="form-control" id="no_id" placeholder="Masukan Nomor ID Pengguna">
               </div>
               <div class="form-group">
                 <label>Perusahaan</label>
-                <select name="id_perusahaan" class="form-control">
+                <select name="perusahaan_id" class="form-control">
                   <option value="">Pilih Perusahaan</option>
                   <option value="1">PT. Huawei Indonesia</option>
                 </select>
