ea89b85786f282276b996243d05d4ec5cd8c9dc9
aepnat <aep.nat@gmail.com>
2018-09-12 21:28:42 +0700

    tampilan data peminjaman kunci

--- admin/lihat/data_peminjaman_kunci.php
+++ admin/lihat/data_peminjaman_kunci.php
@@ -1,1 +1,72 @@
-data peminjaman kunci
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Data</h3>
+
+        <div class="box-tools">
+          <div class="input-group input-group-sm" style="width: 150px;">
+            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+            <div class="input-group-btn">
+              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>ID Kunci</th>
+            <th>Tujuan</th>
+            <th>Peminjam</th>
+            <th>Perusahaan</th>
+            <th>Waktu Pinjam</th>
+            <th>Waktu Selesai</th>
+            <th>Status</th>
+            <th>Aksi</th>
+          </tr>
+          <tr>
+            <td>183</td>
+            <td>Cilandak</td>
+            <td>Poldi</td>
+            <td>PT. Huawei Indonesia</td>
+            <td>02 April 2018 10:00:00</td>
+            <td>02 April 2018 16:00:00</td>
+            <td><span class="label label-success">Selesai</span></td>
+            <td>
+                <button class="btn btn-xs btn-primary">Detail</button>
+                <button class="btn btn-xs btn-success">Edit</button>
+            </td>
+          </tr>
+          <tr>
+            <td>219</td>
+            <td>Manggarai</td>
+            <td>Alexander Pierce</td>
+            <td>PT. Esia Hidayah</td>
+            <td>03 April 2018 10:00:00</td>
+            <td>-</td>
+            <td><span class="label label-warning">Belum</span></td>
+            <td>
+                <button class="btn btn-xs btn-warning">Selesai</button>
+                <button class="btn btn-xs btn-primary">Detail</button>
+                <button class="btn btn-xs btn-success">Edit</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer clearfix">
+        <ul class="pagination pagination-sm no-margin pull-right">
+          <li><a href="#">&laquo;</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">&raquo;</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.box -->
+  </div>
+</div>
