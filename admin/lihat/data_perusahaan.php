42806a33210a1a12daa27b2ca4eb9866e1c262ad
aepnat <aep.nat@gmail.com>
2018-09-12 21:33:08 +0700

    tampilan data perusahaan

--- admin/lihat/data_perusahaan.php
+++ admin/lihat/data_perusahaan.php
@@ -1,1 +1,50 @@
-data perusahaan
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Data</h3>
+
+        <div class="box-tools">
+          <div class="input-group input-group-sm" style="width: 150px;">
+            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+            <div class="input-group-btn">
+              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>Nama</th>
+            <th>No. Telepon</th>
+            <th>Alamat</th>
+            <th>Aksi</th>
+          </tr>
+          <tr>
+            <td>PT. Huawei Indonesia</td>
+            <td>0219343110</td>
+            <td>Jalan Manggarai No. 107 Manggarai Jakarta Timur</td>
+            <td>
+                <button class="btn btn-xs btn-primary">Detail</button>
+                <button class="btn btn-xs btn-success">Edit</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer clearfix">
+        <ul class="pagination pagination-sm no-margin pull-right">
+          <li><a href="#">&laquo;</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">&raquo;</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.box -->
+  </div>
+</div>
