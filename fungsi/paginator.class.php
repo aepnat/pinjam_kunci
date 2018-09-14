3acebc210ab1848294a822aaef073f8928005a3c
aepnat <aep.nat@gmail.com>
2018-09-14 17:25:55 +0700

    Fix url paginator class

--- fungsi/paginator.class.php
+++ fungsi/paginator.class.php
@@ -8,12 +8,14 @@ class Paginator
     private $_page;
     private $_query;
     private $_total;
+    private $_base_url;
 
-    public function __construct( $conn, $query )
+    public function __construct( $conn, $query, $base_url = NULL )
     {
 
        $this->_conn = $conn;
        $this->_query = $query;
+       $this->_base_url = $base_url;
 
        $rs= $this->_conn->query( $this->_query );
        $this->_total = $rs->num_rows;
@@ -57,28 +59,36 @@ class Paginator
         $start      = ( ( $this->_page - $links ) > 0 ) ? $this->_page - $links : 1;
         $end        = ( ( $this->_page + $links ) < $last ) ? $this->_page + $links : $last;
 
+        $base_url   = ($this->_base_url != null) ? $this->_base_url : '';
+
+        if (strpos($base_url, '?') !== false) {
+            $base_url .= '&';
+        } else {
+            $base_url .= '?';
+        }
+
         $html       = '<ul class="' . $list_class . '">';
 
         $class      = ( $this->_page == 1 ) ? "disabled" : "";
-        $html       .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . ( $this->_page - 1 ) . '">&laquo;</a></li>';
+        $html       .= '<li class="' . $class . '"><a href="' . $base_url . 'limit=' . $this->_limit . '&page=' . ( $this->_page - 1 ) . '">&laquo;</a></li>';
 
         if ( $start > 1 ) {
-            $html   .= '<li><a href="?limit=' . $this->_limit . '&page=1">1</a></li>';
+            $html   .= '<li><a href="' . $base_url . 'limit=' . $this->_limit . '&page=1">1</a></li>';
             $html   .= '<li class="disabled"><span>...</span></li>';
         }
 
         for ( $i = $start ; $i <= $end; $i++ ) {
             $class  = ( $this->_page == $i ) ? "active" : "";
-            $html   .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . $i . '">' . $i . '</a></li>';
+            $html   .= '<li class="' . $class . '"><a href="' . $base_url . 'limit=' . $this->_limit . '&page=' . $i . '">' . $i . '</a></li>';
         }
 
         if ( $end < $last ) {
             $html   .= '<li class="disabled"><span>...</span></li>';
-            $html   .= '<li><a href="?limit=' . $this->_limit . '&page=' . $last . '">' . $last . '</a></li>';
+            $html   .= '<li><a href="' . $base_url . 'limit=' . $this->_limit . '&page=' . $last . '">' . $last . '</a></li>';
         }
 
         $class      = ( $this->_page == $last ) ? "disabled" : "";
-        $html       .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . ( $this->_page + 1 ) . '">&raquo;</a></li>';
+        $html       .= '<li class="' . $class . '"><a href="' . $base_url . 'limit=' . $this->_limit . '&page=' . ( $this->_page + 1 ) . '">&raquo;</a></li>';
 
         $html       .= '</ul>';
 
