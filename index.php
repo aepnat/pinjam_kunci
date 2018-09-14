3ac1806427b5faf02e275e53c6f41343d4186c62
aepnat <aep.nat@gmail.com>
2018-09-14 10:18:41 +0700

    login dan logout

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+session_start();
+
+include "config/config.php";
+include "config/database.php";
+
+// kalo sudah login redirect ke admin
+if (isset($_SESSION['pengguna_id'])) {
+    header('Location:' . $config['base_url'] . '/admin');
+}
+
+// periksa kalo ada request untuk login
+if (isset($_POST['method']) && $_POST['method'] == 'login') {
+    // periksa kalo email dan password tidak kosong
+    if (isset($_POST['email']) && isset($_POST['password']) && $_POST['email'] != '' && $_POST['password'] != '') {
+        $stmt = $connectdb->prepare("SELECT * FROM pengguna WHERE email = ? AND password = ?");
+        $stmt->bind_param('ss', $_POST['email'], md5($_POST['password']));
+        $stmt->execute();
+        $hasil = $stmt->get_result();
+        $pengguna = $hasil->fetch_object();
+
+        // kalo pengguna ketemu
+        if ($pengguna) {
+            // simpan session semua data pengguna
+            foreach ($pengguna as $k => $v) {
+                $_SESSION[$k] = $v;
+            }
+
+            // redirect ke admin
+            header('Location:' . $config['base_url'] . '/admin');
+        } else {
+            $error_text = 'Email atau Password salah.';
+        }
+    } else {
+        $error_text = 'Email dan Password tidak boleh kosong';
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -32,18 +71,26 @@
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <form action="ceklogin.php" method="post">
+      <?php if (isset($error_text)):?>
+          <div class="alert alert-danger alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+              <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+              <?php echo $error_text;?>
+          </div>
+      <?php endif;?>
+    <form action="" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" name="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Kata Kunci">
+        <input type="password" name="password" class="form-control" placeholder="Kata Kunci">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Masuk</button>
+          <input type="hidden" name="method" value="login" />
         </div>
         <!-- /.col -->
       </div>
